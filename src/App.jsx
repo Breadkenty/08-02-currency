125393c1fb0d1f51c700246e1575bf9f1de0f992
Kento Kawakami <kawakamikento@gmail.com>
2020-07-04 23:42:15 -0400

    Basic styling and hard coded HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,22 @@
 import React from 'react'
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
-import Page from './pages/Page'
-import Page2 from './pages/Page2'
-import HomePage from './pages/HomePage'
-import NotFound from './pages/NotFound'
-
+import { SelectBase } from './SelectBase'
+import { Results } from './Results'
 function App() {
   return (
-    <Router>
-      <header>
-        <h1>Welcome to my SPA</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Go Home</Link>
-            </li>
-            <li>
-              <Link to="/1">Page 1</Link>
-            </li>
-            <li>
-              <Link to="/2">Page 2</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <Switch>
-        <Route exact path="/" component={HomePage}></Route>
-        <Route exact path="/1" component={Page}></Route>
-        <Route exact path="/2" component={Page2}></Route>
-        <Route path="*" component={NotFound}></Route>
-      </Switch>
-    </Router>
+    <main>
+      <section className="currency-input">
+        <form>
+          <SelectBase />
+          <div>
+            <input name="amount" type="text" placeholder="Amount" />
+          </div>
+          <div>
+            <input name="date" type="date" placeholder="huh" />
+          </div>
+        </form>
+      </section>
+      <Results />
+    </main>
   )
 }
 
---
+++ src/Results.jsx
@@ -0,0 +1,139 @@
+import React from 'react'
+export function Results() {
+  return (
+    <section className="currency-output">
+      <span className="currency-label">GBP:</span>
+      <span>
+        £<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">HKD:</span>
+      <span>
+        HK$<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">IDR:</span>
+      <span>
+        Rp<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">ILS:</span>
+      <span>
+        ₪<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">DKK:</span>
+      <span>
+        <span className="currency-number">10.00</span> kr
+      </span>
+      <span className="currency-label">INR:</span>
+      <span>
+        ₹<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">CHF:</span>
+      <span>
+        <span className="currency-number">10.00</span> francs
+      </span>
+      <span className="currency-label">MXN:</span>
+      <span>
+        $<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">CZK:</span>
+      <span>
+        <span className="currency-number">10.00</span> Kč
+      </span>
+      <span className="currency-label">SGD:</span>
+      <span>
+        $<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">THB:</span>
+      <span>
+        ฿<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">HRK:</span>
+      <span>
+        <span className="currency-number">10.00</span> kn
+      </span>
+      <span className="currency-label">EUR:</span>
+      <span>
+        €<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">MYR:</span>
+      <span>
+        RM<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">NOK:</span>
+      <span>
+        <span className="currency-number">10.00</span> kr
+      </span>
+      <span className="currency-label">CNY:</span>
+      <span>
+        ¥<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">BGN:</span>
+      <span>
+        <span className="currency-number">10.00</span> leva
+      </span>
+      <span className="currency-label">PHP:</span>
+      <span>
+        ₱<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">PLN:</span>
+      <span>
+        <span className="currency-number">10.00</span> zł
+      </span>
+      <span className="currency-label">ZAR:</span>
+      <span>
+        R<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">CAD:</span>
+      <span>
+        $<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">ISK:</span>
+      <span>
+        <span className="currency-number">10.00</span> kr
+      </span>
+      <span className="currency-label">BRL:</span>
+      <span>
+        R$<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">RON:</span>
+      <span>
+        <span className="currency-number">10.00</span> lei
+      </span>
+      <span className="currency-label">NZD:</span>
+      <span>
+        $<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">TRY:</span>
+      <span>
+        ₺<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">JPY:</span>
+      <span>
+        ¥<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">RUB:</span>
+      <span>
+        <span className="currency-number">10.00</span> ₽
+      </span>
+      <span className="currency-label">KRW:</span>
+      <span>
+        ₩<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">USD:</span>
+      <span>
+        $<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">AUD:</span>
+      <span>
+        $<span className="currency-number">10.00</span>
+      </span>
+      <span className="currency-label">HUF:</span>
+      <span>
+        <span className="currency-number">10.00</span> Ft
+      </span>
+      <span className="currency-label">SEK:</span>
+      <span>
+        <span className="currency-number">10.00</span> kr
+      </span>
+    </section>
+  )
+}
